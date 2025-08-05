9358b2e297883dddac442cce540f842f8af4d71b
Adam <goldiron2103@gmail.com>
2025-08-06 05:58:41 +0700

    Update

--- src/pages/Cabang.jsx
+++ src/pages/Cabang.jsx
@@ -1,3 +1,19 @@
-export default function Cabang() {
-  return <h1>Lorem ipsum</h1>;
+import OurServicesSection from "../components/services/Services";
+import Navbar from "../components/Navbar";
+import TopBar from "../components/Topbar";
+import FooterBar from "../components/Footerbar";
+import Footer from "../components/Footer";
+import CustomCTA from "../components/CustomCTA";
+
+export default function Services() {
+  return (
+    <>
+      <TopBar />
+      <Navbar />
+      {/* <OurServicesSection /> */}
+      <CustomCTA />
+      <Footer />
+      <FooterBar />
+    </>
+  );
 }
