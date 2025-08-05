d97febdf9df6a153697dbdec7f16f4beb219b54a
Adam <goldiron2103@gmail.com>
2025-08-06 06:11:48 +0700

    Update

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,3 +1,20 @@
-export default function News() {
-  return <h1>Lorem ipsum</h1>;
+import OurServicesSection from "../components/services/Services";
+import Navbar from "../components/Navbar";
+import TopBar from "../components/Topbar";
+import FooterBar from "../components/Footerbar";
+import Footer from "../components/Footer";
+import CustomCTA from "../components/CustomCTA";
+import Profile from "../components/about/Profile";
+
+export default function Services() {
+  return (
+    <>
+      <TopBar />
+      <Navbar />
+      {/* <OurServicesSection /> */}
+      <CustomCTA />
+      <Footer />
+      <FooterBar />
+    </>
+  );
 }
